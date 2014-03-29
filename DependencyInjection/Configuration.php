1a40c01b0981f2aed6b3bd1aed5f303ed81e3d69
JackLam <linjue@wilead.com>
2014-03-30 01:15:30 +0800

    update

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -20,9 +20,14 @@ class Configuration implements ConfigurationInterface
         $treeBuilder = new TreeBuilder();
         $rootNode = $treeBuilder->root('wiz_httpsqs');
 
-        // Here you should define the parameters that are allowed to
-        // configure your bundle. See the documentation linked above for
-        // more information on that topic.
+        $rootNode
+            ->children()
+                ->arrayNode('queues')
+                    ->isRequired()
+                    ->requiresAtLeastOneElement()
+                    ->prototype('scalar')
+                ->end()
+            ->end();
 
         return $treeBuilder;
     }
--- DependencyInjection/WizHttpsqsExtension.php
+++ DependencyInjection/WizHttpsqsExtension.php
@@ -2,10 +2,13 @@
 
 namespace Wiz\HttpsqsBundle\DependencyInjection;
 
+use HTTPSQS\Queue;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\Definition;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader;
+use Symfony\Component\PropertyAccess\PropertyAccess;
 
 /**
  * This is the class that loads and manages your bundle configuration
@@ -22,7 +25,13 @@ class WizHttpsqsExtension extends Extension
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
 
-        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-        $loader->load('services.yml');
+        $propertyAccess = PropertyAccess::createPropertyAccessor();
+
+        $queues = $propertyAccess->getValue($config, '[queues]');
+        foreach ($queues as $queue) {
+            $serviceId = 'wiz_httpsqs.queue.' . $queue;
+            $definition = new Definition('HTTPSQS\Queue', array($queue));
+            $container->setDefinition($serviceId, $definition);
+        }
     }
 }
