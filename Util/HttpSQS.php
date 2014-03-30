ddd7d43f3cc8f081d90a0c99152e97c5485b6aa8
JackLam <linjue@wilead.com>
2014-03-30 17:06:21 +0800

    update

---
+++ Util/HttpSQS.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * File: HttpSQS.php.
+ * User: JackLam<jack@wilead.com>
+ */
+
+namespace Wiz\HttpsqsBundle\Util;
+
+class HttpSQS
+{
+    const HTTPSQS_GET_END = 'HTTPSQS_GET_END';
+} 
